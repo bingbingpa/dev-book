760e3d47581d7fa6eb04e8937e36edd79dd7b44a
shpark <guriguri1576@gmail.com>
2019-11-12 00:23:37 +0900

    gradle build 자바버전 11로 변경,
    stream 테스트

---
+++ src/main/java/com/ch08/CountLongWords.java
@@ -0,0 +1,28 @@
+package com.ch08;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLongWords {
+    public static void main(String[] args) throws IOException {
+        String contents = new String(Files.readAllBytes(Paths.get("src/main/resources/alice.txt")),
+                StandardCharsets.UTF_8);
+        List<String> words = List.of(contents.split("\\PL+"));
+
+        long count = 0;
+        for (String w : words) {
+            if (w.length() > 12)
+                count++;
+        }
+        System.out.println(count);
+
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+    }
+}
