ec781029c54e72b1897a3d52730b625944991612
shpark <bingbingpa@gmail.com>
2020-01-19 00:47:13 +0900

    5장 step2. DisCountCondition개선(변경 취약점 개선)

---
+++ src/main/java/com/bingbingpa/ch05/movie/step02/SequenceCondition.java
@@ -0,0 +1,13 @@
+package com.bingbingpa.ch05.movie.step02;
+
+public class SequenceCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
