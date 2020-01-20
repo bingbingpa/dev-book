1e9a5f06eb27961c6c7f5731c9fc9c3673d8133e
bingbingpa <bingbingpa@gmail.com>
2020-01-20 10:35:16 +0900

    5장 step4. 진행중

---
+++ src/main/java/com/bingbingpa/ch05/movie/step04/SequenceCondition.java
@@ -0,0 +1,13 @@
+package com.bingbingpa.ch05.movie.step04;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
