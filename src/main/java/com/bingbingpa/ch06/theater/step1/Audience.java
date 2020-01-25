e44e4c268f32e532b5ae84776ce5c419c43aa9d0
shpark <bingbingpa@gmail.com>
2020-01-26 01:59:41 +0900

    6장 step1

---
+++ src/main/java/com/bingbingpa/ch06/theater/step1/Audience.java
@@ -0,0 +1,13 @@
+package com.bingbingpa.ch06.theater.step1;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
