e18ce2c1c49726a1d68bf669fd676e9e92f838b6
shpark <bingbingpa@gmail.com>
2020-02-06 00:21:58 +0900

    10장 상속과 코드 재사용 시작. step01

---
+++ src/main/java/com/bingbingpa/ch10/billing/step01/NightlyDiscountPhone.java
@@ -0,0 +1,36 @@
+package com.bingbingpa.ch10.billing.step01;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bingbingpa.ch10.money.Money;
+
+public class NightlyDiscountPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+        }
+
+        return result;
+    }
+}
