0259a21bf35c576846b603c1f312eb10459f7bec
shpark <bingbingpa@gmail.com>
2020-02-22 00:55:18 +0900

    누락 커밋

--- src/main/java/com/bingbingpa/ch14/billing/step01/ReatePolicy.java
+++
@@ -1,8 +0,0 @@
-package com.bingbingpa.ch14.billing.step01;
-
-import com.bingbingpa.ch10.billing.step01.Phone;
-import com.bingbingpa.ch10.money.Money;
-
-public interface ReatePolicy {
-	Money calculateFee(Phone phone);
-}
