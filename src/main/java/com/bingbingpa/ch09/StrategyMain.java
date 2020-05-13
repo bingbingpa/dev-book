f05adf09d322055633730a534c83d3d48ff6a948
shpark <bingbingpa@gmail.com>
2020-05-14 00:09:50 +0900

    9장

---
+++ src/main/java/com/bingbingpa/ch09/StrategyMain.java
@@ -0,0 +1,55 @@
+package com.bingbingpa.ch09;
+
+public class StrategyMain {
+
+    public static void main(String[] args) {
+        // old school
+        Validator v1 = new Validator(new IsNumeric());
+        System.out.println(v1.validate("aaaa"));
+        Validator v2 = new Validator(new IsAllLowerCase());
+        System.out.println(v2.validate("bbbb"));
+
+        // with lambdas
+        Validator v3 = new Validator((String s) -> s.matches("\\d+"));
+        System.out.println(v3.validate("aaaa"));
+        Validator v4 = new Validator((String s) -> s.matches("[a-z]+"));
+        System.out.println(v4.validate("bbbb"));
+    }
+
+    interface ValidationStrategy {
+        boolean execute(String s);
+    }
+
+    private static class IsAllLowerCase implements ValidationStrategy {
+
+        @Override
+        public boolean execute(String s) {
+            return s.matches("[a-z]+");
+        }
+
+    }
+
+    private static class IsNumeric implements ValidationStrategy {
+
+        @Override
+        public boolean execute(String s) {
+            return s.matches("\\d+");
+        }
+
+    }
+
+    private static class Validator {
+
+        private final ValidationStrategy strategy;
+
+        public Validator(ValidationStrategy v) {
+            strategy = v;
+        }
+
+        public boolean validate(String s) {
+            return strategy.execute(s);
+        }
+
+    }
+
+}
