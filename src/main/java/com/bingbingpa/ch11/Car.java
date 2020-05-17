c23d64c4a23460f6e21aba35aa63ca2ab4221a94
shpark <bingbingpa@gmail.com>
2020-05-18 00:00:46 +0900

    Optional 테스트를 위한 도메인 추가

---
+++ src/main/java/com/bingbingpa/ch11/Car.java
@@ -0,0 +1,13 @@
+package com.bingbingpa.ch11;
+
+import java.util.Optional;
+
+public class Car {
+
+    private Optional<Insurance> insurance;
+
+    public Optional<Insurance> getInsurance() {
+        return insurance;
+    }
+
+}
---
+++ src/main/java/com/bingbingpa/ch11/Person.java
@@ -0,0 +1,18 @@
+package com.bingbingpa.ch11;
+
+import java.util.Optional;
+
+public class Person {
+
+    private Optional<Car> car;
+    private int age;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
