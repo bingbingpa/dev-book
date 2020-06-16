3e9a7664a0e36c9d8a0c43256b1277b3eaaca362
shpark <bingbingpa@gmail.com>
2020-06-17 00:42:27 +0900

    15장 실

---
+++ src/main/java/com/bingbingpa/ch15/CFComplete.java
@@ -0,0 +1,23 @@
+package com.bingbingpa.ch15;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.bingbingpa.ch15.Functions.f;
+import static com.bingbingpa.ch15.Functions.g;
+
+public class CFComplete {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        int x = 1337;
+
+        CompletableFuture<Integer> a = new CompletableFuture<>();
+        executorService.submit(() -> a.complete(f(x)));
+        int b = g(x);
+        System.out.println(a.get() + b);
+
+        executorService.shutdown();
+    }
+}
