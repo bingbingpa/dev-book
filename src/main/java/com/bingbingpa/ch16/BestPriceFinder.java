901be4bf47c483e1269004a559731cad684474d5
shpark <bingbingpa@gmail.com>
2020-06-23 01:36:10 +0900

    임시

---
+++ src/main/java/com/bingbingpa/ch16/BestPriceFinder.java
@@ -0,0 +1,14 @@
+package com.bingbingpa.ch16;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BestPriceFinder {
+
+    private final List<Shop> shops = Arrays.asList(
+            new Shop("BestPrice"),
+            new Shop("LetsSaveBig"),
+            new Shop("MyFavoriteShop"),
+            new Shop("BuyItAll"),
+            new Shop("ShopEasy"));
+}
