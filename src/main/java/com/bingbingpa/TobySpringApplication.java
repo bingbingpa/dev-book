a6b62f875880d18ab454eef2094fae9cb67474be
shpark <bingbingpa@gmail.com>
2020-09-13 23:28:56 +0900

    init

---
+++ src/main/java/com/bingbingpa/TobySpringApplication.java
@@ -0,0 +1,13 @@
+package com.bingbingpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bingbingpa/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingbingpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
