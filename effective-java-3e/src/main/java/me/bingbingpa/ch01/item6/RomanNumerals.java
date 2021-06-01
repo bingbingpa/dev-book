254bd56af8c94988068c809c000d5e7c34a62cb0
shpark <bingbingpa@gmail.com>
2021-06-01 20:35:36 +0900

    test(이펙티브자바3판): 아이템 4,5,6

---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch01/item6/RomanNumerals.java
@@ -0,0 +1,38 @@
+package me.bingbingpa.ch01.item6;
+
+import java.util.regex.Pattern;
+
+public class RomanNumerals {
+    static boolean isRomanNumeralSlow(String s) {
+        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+                + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeralFast(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+
+    public static void main(String[] args) {
+        int numSets = 1000;
+        int numReps = 1000;
+        boolean b = false;
+
+        for (int i = 0; i < numSets; i++) {
+            long start = System.nanoTime();
+            for (int j = 0; j < numReps; j++) {
+                // 성능 차이를 확인하려면 xxxSlow 메서드를 xxxFast 메서드로 바꿔 실행해보자.
+                b ^= isRomanNumeralSlow("MCMLXXVI");
+            }
+            long end = System.nanoTime();
+            System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
+        }
+
+        // VM이 최적화하지 못하게 막는 코드
+        if (!b)
+            System.out.println();
+    }
+}
---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch01/item6/Sum.java
@@ -0,0 +1,19 @@
+package me.bingbingpa.ch01.item6;
+
+public class Sum {
+    private static long sum() {
+        long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            long start = System.nanoTime();
+            sum();
+            long end = System.nanoTime();
+            System.out.println((end - start) / 1_000_000. + " ms.");
+        }
+    }
+}
