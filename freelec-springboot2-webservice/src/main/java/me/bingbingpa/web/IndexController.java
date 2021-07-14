9450abe69852ca76f79e5d7880fa77196eb38fd5
shpark <bingbingpa@gmail.com>
2021-07-14 23:55:38 +0900

    스프링 부트와 AWS 로 혼자 구현하는 웹 서비스: 5장- LoginUserArgumentResolver 적용

--- freelec-springboot2-webservice/src/main/java/me/bingbingpa/web/IndexController.java
+++ freelec-springboot2-webservice/src/main/java/me/bingbingpa/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.bingbingpa.web;
 
 import lombok.RequiredArgsConstructor;
+import me.bingbingpa.config.auth.LoginUser;
 import me.bingbingpa.config.auth.dto.SessionUser;
 import me.bingbingpa.service.PostsService;
 import me.bingbingpa.web.dto.PostResponseDto;
@@ -9,19 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
