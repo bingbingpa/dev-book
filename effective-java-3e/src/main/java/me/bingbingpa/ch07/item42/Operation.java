556992f2dee3401d11746bb843a66b0aa7caa9dd
shpark <bingbingpa@gmail.com>
2021-07-25 20:07:06 +0900

    이펙티브자바3판: 42, 43, 44

---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch07/item42/Operation.java
@@ -0,0 +1,34 @@
+package me.bingbingpa.ch07.item42;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operation {
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
+
+    private final String symbol;
+    private final DoubleBinaryOperator op;
+
+    Operation(String symbol, DoubleBinaryOperator op) {
+        this.symbol = symbol;
+        this.op = op;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
+    public double apply(double x, double y) {
+        return op.applyAsDouble(x, y);
+    }
+
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 20;
+        for (Operation op : Operation.values())
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+    }
+}
