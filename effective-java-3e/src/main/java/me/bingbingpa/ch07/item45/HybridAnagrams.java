f22ea7fdc464019dbc3eff64b0e196361573b05a
shpark <bingbingpa@gmail.com>
2021-07-29 18:03:20 +0900

    이펙티브자바3판: item 45, 46, 47, 48

---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch07/item45/HybridAnagrams.java
@@ -0,0 +1,30 @@
+package me.bingbingpa.ch07.item45;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class HybridAnagrams {
+    public static void main(String[] args) throws IOException {
+        Path dictionary = Paths.get(args[0]);
+        int minGroupSize = Integer.parseInt(args[1]);
+
+        try (Stream<String> words = Files.lines(dictionary)) {
+            words.collect(groupingBy(HybridAnagrams::alphabetize))
+                    .values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .forEach(g -> System.out.println(g.size() + ": " + g));
+        }
+    }
+
+    private static String alphabetize(String s) {
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
+    }
+}
---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch07/item47/PowerSet.java
@@ -0,0 +1,34 @@
+package me.bingbingpa.ch07.item47;
+
+import java.util.*;
+
+public class PowerSet {
+    public static final <E> Collection<Set<E>> of(Set<E> s) {
+        List<E> src = new ArrayList<>(s);
+        if (src.size() > 30) {
+            throw new IllegalArgumentException("집합에 원소가 너무 많습니다.(최대 30개): " + s);
+        }
+        return new AbstractList<Set<E>>() {
+            @Override
+            public Set<E> get(int index) {
+                Set<E> result = new HashSet<>();
+                for (int i = 0; index != 0; i++, index >>= 1) {
+                    if ((index & 1) == 1) {
+                        result.add(src.get(i));
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public int size() {
+                return 1 << src.size();
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return o instanceof Set && src.containsAll((Set) o);
+            }
+        };
+    }
+}
