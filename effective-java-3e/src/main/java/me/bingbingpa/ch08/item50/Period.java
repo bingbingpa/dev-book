b6469ec9d56124627868b6bf9057ef8c2e3a918c
shpark <bingbingpa@gmail.com>
2021-08-02 17:31:30 +0900

    이펙티브자바3판: item 49, 50, 51

---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch08/item50/Period.java
@@ -0,0 +1,28 @@
+package me.bingbingpa.ch08.item50;
+
+import java.util.Date;
+
+public final class Period {
+    private final Date start;
+    private final Date end;
+
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+
+        if (this.start.compareTo(this.end) > 0)
+            throw new IllegalArgumentException(this.start + "가 " + this.end + "보다 늦다.");
+    }
+
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    public String toString() {
+        return start + " - " + end;
+    }
+}
