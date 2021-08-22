121664d325d3e155de56ed7fcebaf0cc29dfd4d9
shpark <bingbingpa@gmail.com>
2021-08-22 21:32:25 +0900

    이펙티브자바3판: item 78

---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch11/item78/brokenstopthread/StopThread.java
@@ -0,0 +1,20 @@
+package me.bingbingpa.ch11.item78.brokenstopthread;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThread {
+    private static boolean stopRequested;
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested)
+                i++;
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+}
---
+++ effective-java-3e/src/main/java/me/bingbingpa/ch11/item78/fixedstopthread2/StopThread.java
@@ -0,0 +1,20 @@
+package me.bingbingpa.ch11.item78.fixedstopthread2;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThread {
+    private static volatile boolean stopRequested;
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested)
+                i++;
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+}
