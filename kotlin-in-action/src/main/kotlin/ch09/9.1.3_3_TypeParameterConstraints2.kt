f06ca538c8974cf06b72e87380eb80c28d297431
shpark <bingbingpa@gmail.com>
2022-01-25 00:44:01 +0900

    kotlin-in-action: 9장 제네릭스 ing...

---
+++ kotlin-in-action/src/main/kotlin/ch09/9.1.3_3_TypeParameterConstraints2.kt
@@ -0,0 +1,14 @@
+package ch09.ex1_3_3_TypeParameterConstraints2
+
+fun <T> ensureTrailingPeriod(seq: T)
+        where T : CharSequence, T : Appendable {
+    if (!seq.endsWith('.')) {
+        seq.append('.')
+    }
+}
+
+fun main() {
+    val helloWorld = StringBuilder("Hello World")
+    ensureTrailingPeriod(helloWorld)
+    println(helloWorld)
+}
