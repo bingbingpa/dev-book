4eb0bfb89d4dbd32814f73c021b7416f61b581f0
shpark <bingbingpa@gmail.com>
2022-01-27 00:18:24 +0900

    kotlin-in-action: 9장 제네릭스

---
+++ kotlin-in-action/src/main/kotlin/ch09/9.3.6.2_3_FieldValidator.kt
@@ -0,0 +1,21 @@
+package ch09.FieldValidator
+
+import kotlin.reflect.KClass
+
+interface FieldValidator<in T> {
+    fun validate(input: T): Boolean
+}
+
+object DefaultStringValidator : FieldValidator<String> {
+    override fun validate(input: String): Boolean = input.isNotEmpty()
+}
+
+object DefaultIntValidator : FieldValidator<Int> {
+    override fun validate(input: Int): Boolean = input >= 0
+}
+
+fun main() {
+    val validators = mutableMapOf<KClass<*>, FieldValidator<*>>()
+    validators[String::class] = DefaultStringValidator
+    validators[Int::class] = DefaultIntValidator
+}
