dc945a4d87657504196696d99b5793c168708712
shpark <bingbingpa@gmail.com>
2022-02-02 20:53:14 +0900

    kotlin-in-action: 11장 DSL 만들기. kotlin in action 끝!!!

---
+++ kotlin-in-action/src/main/kotlin/ch11/11.3.3_InvokeConventionInDSLs.kt
@@ -0,0 +1,19 @@
+package ch11.ex3_3_InvokeConventionInDSLs
+
+class DependencyHandler {
+    fun compile(coordinate: String) {
+        println("Added dependency on $coordinate")
+    }
+
+    operator fun invoke(body: DependencyHandler.() -> Unit) {
+        body()
+    }
+}
+
+fun main() {
+    val dependencies = DependencyHandler()
+    dependencies.compile("org.jetbrains.kotlin:kotlin-stdlib:1.0.0")
+    dependencies {
+        compile("org.jetbrains.kotlin:kotlin-reflect:1.0.0")
+    }
+}
