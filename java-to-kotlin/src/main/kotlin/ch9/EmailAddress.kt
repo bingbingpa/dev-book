f5c91040cf7aaf26488ee54e0f6f864a2a034fc0
shpark <bingbingpa@gmail.com>
2022-12-18 21:24:54 +0900

    java-to-kotlin: 9장 정리

---
+++ java-to-kotlin/src/main/kotlin/ch9/EmailAddress.kt
@@ -0,0 +1,26 @@
+package ch9
+
+data class EmailAddress(
+    val localPart: String,
+    val domain: String
+) {
+
+    override fun toString() = "$localPart@$domain"
+
+    companion object {
+        @JvmStatic
+        fun parse(value: String): EmailAddress {
+            return value.splitAroundLast('@').let { (leftPart, rightPart) ->
+                EmailAddress(leftPart, rightPart)
+            }
+        }
+    }
+}
+
+private fun String.splitAroundLast(divider: Char): Pair<String, String> =
+    lastIndexOf(divider).let { index ->
+        require(index >= 1 && index != length - 1) {
+            "string must be two non-empty parts separated by $divider"
+        }
+        substring(0, index) to substring(index + 1)
+    }
